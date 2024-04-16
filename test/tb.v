4a11a7f981ef105a9b8f50e2fa4ffca0913e975d
HHRB98 <167050019+HHRB98@users.noreply.github.com>
2024-04-16 00:06:59 -0600

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -16,26 +16,28 @@ module tb ();
   reg clk;
   reg rst_n;
   reg ena; 
-  reg [3:0] ui_in;
-  reg [3:0] uio_in;
+  reg [7:0] ui_in;
+  reg [7:0] uio_in;
   wire [7:0] uo_out;
   wire [7:0] uio_out;
   wire [7:0] uio_oe;
  
 // Replace tt_um_example with your module name:
 tt_um_array_multiplier_hhrb98 user_project (
-    // Include power ports for the Gate Level test:
    
+      // Include power ports for the Gate Level test:
 `ifdef GL_TEST
       .VPWR(1'b1),
       .VGND(1'b0),
 `endif
-   
-    .a(ui_in),    // Connect ui_in directly to module's a
-    .b(uio_in),   // Connect uio_in directly to module's b
-    .clk(clk),    // Connect clk directly to module's clk
-    .ena(ena),    // Connect ena directly to module's ena
-    .rst_n(rst_n),// Connect rst_n directly to module's rst_n
-    .p(uo_out)    // Connect uo_out directly to module's p
-);
+
+      .ui_in  (ui_in),    // Dedicated inputs
+      .uo_out (uo_out),   // Dedicated outputs
+      .uio_in (uio_in),   // IOs: Input path
+      .uio_out(uio_out),  // IOs: Output path
+      .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
+      .ena    (ena),      // enable - goes high when design is selected
+      .clk    (clk),      // clock
+      .rst_n  (rst_n)     // not reset
+  );
 endmodule
