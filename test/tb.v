4a609e2e6e2a096d50b750767f2bd5ea48f2931d
HHRB98 <167050019+HHRB98@users.noreply.github.com>
2024-04-15 22:33:50 -0600

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -16,12 +16,10 @@ module tb ();
   reg clk;
   reg rst_n;
   reg ena;
-  reg [7:0] ui_in;
-  reg [7:0] uio_in;
-  wire [7:0] uo_out;
-  wire [7:0] uio_out;
-  wire [7:0] uio_oe;
-
+  reg [3:0] a;
+  reg [3:0] b;
+  reg [7:0] p;
+ 
   // Replace tt_um_example with your module name:
   tt_um_array_multiplier_hhrb98 tt_um_array_multiplier_hhrb98(
 
@@ -31,14 +29,9 @@ module tb ();
       .VGND(1'b0),
 `endif
 
-      .ui_in  (ui_in),    // Dedicated inputs
-      .uo_out (uo_out),   // Dedicated outputs
-      .uio_in (uio_in),   // IOs: Input path
-      .uio_out(uio_out),  // IOs: Output path
-      .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
-      .ena    (ena),      // enable - goes high when design is selected
-      .clk    (clk),      // clock
-      .rst_n  (rst_n)     // not reset
-  );
+      .a  (a),    // Dedicated inputs
+      .b (b),   // Dedicated outputs
+      .p (p),   // IOs: Input path
+        );
 
 endmodule
