d4f9dcc5a90ba123ea1f971d1d18c2752d60869c
HHRB98 <167050019+HHRB98@users.noreply.github.com>
2024-04-17 00:05:21 -0600

    Update tt_um_array_multiplier_hhrb98.v

--- src/tt_um_array_multiplier_hhrb98.v
+++ src/tt_um_array_multiplier_hhrb98.v
@@ -17,20 +17,27 @@ module tt_um_array_multiplier_hhrb98 (
     output wire [7:0] uio_out,  // IOs: Output path
     output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
     input wire        clk,
-    input  wire       ena,      /* verilator lint_off UNUSEDSIGNAL */
+    //input  wire       ena,      /* verilator lint_off UNUSEDSIGNAL */
     input  wire       rst_n     
   );
   // Wires
  
-  always @(posedge clk) begin  
+// Wires for other signals
+wire [7:0] uio_out_wire;
+
+// Assign uio_out_wire to uio_out
+assign uio_out = uio_out_wire;
+
+// Always block to control uio_out_wire
+always @(posedge clk or negedge rst_n) begin  
     if (~rst_n) begin
-      // Reset condition: set uio_out to 0
-      uio_out <= 8'b0;
+        // Reset condition: set uio_out_wire to 0
+        uio_out_wire <= 8'b0;
     end else begin
-      // Update uio_out with uio_in value
-      uio_out <= uio_in;
+        // Update uio_out_wire with uio_in value
+        uio_out_wire <= uio_in;
     end
-  end
+end
 
   assign uio_oe =8'b0;
   wire [39:0] w;
