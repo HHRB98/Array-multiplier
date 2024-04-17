7ba4c5744ad1fba8ec9e4c3e0c4742f158c8752b
HHRB98 <167050019+HHRB98@users.noreply.github.com>
2024-04-16 20:06:18 -0600

    Update tt_um_array_multiplier_hhrb98.v

--- src/tt_um_array_multiplier_hhrb98.v
+++ src/tt_um_array_multiplier_hhrb98.v
@@ -13,8 +13,8 @@ endmodule
 module tt_um_array_multiplier_hhrb98 (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire uio_in,   // IOs: Input path
-    output wire  uio_out,  // IOs: Output path
+    input  wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
     output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
     input wire clk,
     input  wire       ena,      // will go high when the design is enabled
